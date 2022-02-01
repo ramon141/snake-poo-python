8df37e0eff4ffad058bdf681f68ab6c660ef5087
ramon141 <ramonbarbosa123456789@gmail.com>
2022-02-01 20:03:52 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+largura = 3
+altura = 3
+
+matriz = []
+
+for i in range(largura):
+  listAltura = []
+  for j in range(altura):
+    listAltura.append(0)
+  matriz.append(listAltura)
+
+
+print(matriz)
